2268da693182dd86ecca8e7ac8763df3303ad9bb
Xusniddin <narkulof@gmail.com>
2022-09-14 14:28:40 +0500

    sidebar style completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import SideBar from "./SideBar";
+import Videos from "./Videos";
 
 const Feed = () => {
-   return <div>Feed</div>;
+   return (
+      <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+         <Box
+            sx={{
+               height: { sx: "auto", md: "92vh" },
+               borderRight: "1px solid #3d3d3d",
+               px: { sx: 0, md: 2 },
+            }}
+         >
+            <SideBar />
+
+            <Typography
+               className="copyright"
+               variant="body2"
+               sx={{ mt: 1.5, color: "#fff" }}
+            >
+               Copyright 2022
+            </Typography>
+         </Box>
+
+         <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+            <Typography
+               variant="h5"
+               fontWeight="bold"
+               mb={2}
+               sx={{ color: "white" }}
+            >
+               New <span style={{ color: "#f31503" }}>Videos</span>
+            </Typography>
+
+            <Videos />
+         </Box>
+      </Stack>
+   );
 };
 
 export default Feed;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Stack } from "@mui/material";
+
+import { categories } from "../utils/constants";
+
+const SideBar = () => {
+   const [selectedCategory, setSelectedCategory] = useState("New");
+   return (
+      <Stack
+         direction="row"
+         sx={{
+            overflowY: "auto",
+            height: { sx: "auto", md: "95%" },
+            flexDirection: { md: "column" },
+         }}
+      >
+         {categories.map(({ icon, name }) => (
+            <button
+               className="category-btn"
+               style={{
+                  background: name === selectedCategory && "#fc1503",
+                  color: "white",
+               }}
+               onClick={() => {
+                  setSelectedCategory(name);
+               }}
+            >
+               <span
+                  style={{
+                     color: name === selectedCategory ? "white" : "red",
+                     marginRight: "15px",
+                  }}
+               >
+                  {icon}
+               </span>
+               <span>{name}</span>
+            </button>
+         ))}
+      </Stack>
+   );
+};
+
+export default SideBar;
